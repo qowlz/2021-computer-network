8366a1e8881dc336386c64fdc1997f1a43eb3a22
201702083 <201702083@o.cnu.ac.kr>
2021-10-20 00:21:49 +0900

    rm test code

--- ARP/src/arp/EthernetLayer.java
+++ ARP/src/arp/EthernetLayer.java
@@ -13,22 +13,17 @@ public class EthernetLayer extends BaseLayer {
 	}
 	
 	@Override
-	public boolean Send(byte[] input) {	
-
-		Header.data = input; // 데이터 넣기 
+	public boolean Send(byte[] input) {
+		// log("sending packet to under layer");
+		
+		Header.data = input; // 데이터 넣기
 		Header.mac_src = Arrays.copyOf(macAddress,6); // 송신지 설정	
 		
+		
 		if (Header.frame_type == 0x0800) { // ip에서 날라온거
-			IP_HEADER UpperHeader = ByteToObj(input, IP_HEADER.class);
+			System.out.println("IP Send Packet");
 			System.out.println(Header.frame_type);
-
-			IP_HEADER IPTEST = ByteToObj(input, IP_HEADER.class);
-			TCP_HEADER test = ByteToObj(IPTEST.data,TCP_HEADER.class);
-			System.out.println(test.port_dst);
-			System.out.println(test.port_src);
-			this.GetUpperLayer(1).Receive(Header.data);
-			
-			
+			System.out.println(MacToStr(Header.mac_src) + " -> " + MacToStr(Header.mac_dst));
 			this.GetUnderLayer(0).Send(ObjToByte(Header));
 			return true;
 			
@@ -38,35 +33,41 @@ public class EthernetLayer extends BaseLayer {
 			Header.frame_type = 0x0806; // arp 타입
 
 			switch(UpperHeader.opcode) { // arp 헤더의 옵코드
-			case 1: // ARP request
-				Header.mac_dst = new byte[] {-1, -1, -1, -1, -1, -1}; // 브로드 캐스팅
-				break;
-			case 2: // ARP reply
-				Header.mac_dst = Arrays.copyOf(UpperHeader.mac_dst,6); // 수신지 맥주소는 인계받음
-				break;
+				case 1: // ARP request
+					Header.mac_dst = new byte[] {-1, -1, -1, -1, -1, -1}; // 브로드 캐스팅
+					break;
+				case 2: // ARP reply
+					Header.mac_dst = Arrays.copyOf(UpperHeader.mac_dst,6); // 수신지 맥주소는 인계받음
+					break;
 			}
+			System.out.println("ARP Send Packet");
+			System.out.println(Header.frame_type);
+			System.out.println(MacToStr(Header.mac_src) + " -> " + MacToStr(Header.mac_dst));
 			
 			this.GetUnderLayer(0).Send(ObjToByte(Header));
 			return true;
 		}
-
 	}
 	
-	public boolean Receive(byte[] input) {
+	public synchronized boolean Receive(byte[] input) {
+
 
 		Header = ByteToObj(input, ETHERNET_HEADER.class);
 		
 		if (Arrays.equals(Header.mac_src, macAddress))
 			return false;
 		
-		System.out.println(MacToStr(Header.mac_src) + " -ETHER RECV> " + MacToStr(Header.mac_dst));
+		System.out.println(MacToStr(Header.mac_src) + " -> " + MacToStr(Header.mac_dst));
 		
 		if( (isBroadcast(Header.mac_dst) || Arrays.equals(Header.mac_dst, macAddress))) { // 수신지가 브로드캐스팅 이거나 나 일경우 수신
 			System.out.println(Header.frame_type);
 			if(Header.frame_type == 0x0806){ // arp 타입이면
+				System.out.println("To ARP Layer");
 				this.GetUpperLayer(0).Receive(Header.data);
 				return true;
 			}else if(Header.frame_type == 0x0800) { // IP 타입이면
+				System.out.println(MacToStr(Header.mac_src) + " -ETHER RECV> " + MacToStr(Header.mac_dst));
+				System.out.println("이더넷 IP 받음" + GetUpperLayer(1).GetLayerName() + "으로 전송");
 				this.GetUpperLayer(1).Receive(Header.data);
 				return true;
 			}

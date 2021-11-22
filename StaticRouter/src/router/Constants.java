d81626f630fa28b5e64b176d3bd04837647fc19a
byunguk.choi <qowlz45@gmail.com>
2021-11-23 04:42:46 +0900

    Add constant class

---
+++ StaticRouter/src/router/Constants.java
@@ -0,0 +1,9 @@
+package router;
+
+public final class Constants {
+	public static final String NILayerName = "NI";
+	public static final String EthLayerName = "Eth";
+	public static final String IPLayerName = "IP";
+	public static final String ARPLayerName = "ARP";
+	public static final String AppLayerName = "APP";
+}

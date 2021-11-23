51023b701623cf66a1cf62c67cbfdb096236b5ce
melemo2 <sonmingi0123@gmail.com>
2021-11-23 19:36:05 +0900

    충돌해결

--- StaticRouter/src/router/ARPLayer.java
+++ StaticRouter/src/router/ARPLayer.java
@@ -29,15 +29,6 @@ public class ARPLayer extends BaseLayer{
 		IP_HEADER UpperHeader = ByteToObj(input, IP_HEADER.class);
 		
 		SendHeader.ip_dst = Arrays.copyOf(UpperHeader.ip_dst, 4);
-<<<<<<< HEAD
-		SendHeader.opcode = 0x01; // request Ÿ��
-		SendHeader.ip_src = Arrays.copyOf(ipAddress,4);
-		SendHeader.mac_src = Arrays.copyOf(macAddress,6); // �۽��� �������� ����
-		SendHeader.mac_dst = new byte[] {-1,-1,-1,-1,-1,-1}; // ������ ��ε�ĳ����
-		// ������ ip�� arpapp layer���� ����
-		
-		if(getCache(SendHeader.ip_dst) == null && Arrays.equals(SendHeader.ip_src, SendHeader.ip_dst) != true) { // ����� ������ �ּҰ� ���ų� ���ο��� �����°� ����
-=======
 		SendHeader.opcode = 0x01; // request 타입
 		SendHeader.ip_src = Arrays.copyOf(ipAddress,4);
 		SendHeader.mac_src = Arrays.copyOf(macAddress,6); // 송신지 본인으로 설정
@@ -45,7 +36,6 @@ public class ARPLayer extends BaseLayer{
 		// 목적지 ip는 arpapp layer에서 설정
 		
 		if(getCache(SendHeader.ip_dst) == null && Arrays.equals(SendHeader.ip_src, SendHeader.ip_dst) != true) { // 헤더에 목적지 주소가 없거나 본인에게 보내는거 제외
->>>>>>> af3553c4aeee5a61f6c1f75e0bb014e562284b8d
 			byte[] tempMac = new byte[6];
 			ARP_CACHE arpcache = new ARP_CACHE(SendHeader.ip_dst, tempMac, false);
 			addCacheTable(arpcache);
@@ -63,28 +53,7 @@ public class ARPLayer extends BaseLayer{
 		RecvHeader = ByteToObj(input, ARP_HEADER.class);
 		
 		ARP_CACHE tempARP = getCache(RecvHeader.ip_src);
-<<<<<<< HEAD
-		if(Arrays.equals(RecvHeader.ip_src, ipAddress)) return false; // �۽����� �����̸� ����
-
-		if(tempARP == null) { // ĳ�����̺� ������
-			ARP_CACHE arpCache = new ARP_CACHE(RecvHeader.ip_src, RecvHeader.mac_src, true);
-			addCacheTable(arpCache); // ���� ���� �߰�
-		}else {  // ĳ�����̺� ����
-			tempARP.status = true;
-			tempARP.mac = Arrays.copyOf(RecvHeader.mac_src, 6); // ���Ź��� �������� �۽����� �����
-		}
-		updateCacheTable();
-
-		switch (RecvHeader.opcode) { // arp ��Ŷ ���ڵ�� �з�
-		case 0x01: // request
-
-			if(Arrays.equals(RecvHeader.ip_dst, ipAddress)) {	// �������� �����̸�
-				SendHeader.opcode = 0x02; // reply
-				SendHeader.ip_dst = Arrays.copyOf(RecvHeader.ip_src,4); // �������� ��������
-				SendHeader.mac_dst = Arrays.copyOf(RecvHeader.mac_src,6); // �������� ��������
-				SendHeader.mac_src = macAddress; // �۽��� �ٽ�����
-				SendHeader.ip_src = ipAddress; // �۽��� ip ���� ����
-=======
+<
 		if(Arrays.equals(RecvHeader.ip_src, ipAddress)) return false; // 송신지가 본인이면 종료
 
 		if(tempARP == null) { // 캐시테이블 미적중
@@ -105,18 +74,12 @@ public class ARPLayer extends BaseLayer{
 				SendHeader.mac_dst = Arrays.copyOf(RecvHeader.mac_src,6); // 수신지를 목적지로
 				SendHeader.mac_src = macAddress; // 송신지 다시지정
 				SendHeader.ip_src = ipAddress; // 송신지 ip 나로 지정
->>>>>>> af3553c4aeee5a61f6c1f75e0bb014e562284b8d
 				GetUnderLayer(0).Send(ObjToByte(SendHeader));
 			}
 			break;
 		case 0x02: // reply
-<<<<<<< HEAD
-			System.out.println("ARP ���� ����");
-			// ���� �Ϸ�
-=======
 			System.out.println("ARP 응답 수신");
 			// 수신 완료
->>>>>>> af3553c4aeee5a61f6c1f75e0bb014e562284b8d
 			break;
 		}
 		return true;

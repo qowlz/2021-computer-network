94238b9825c9929e1278ff61fab2ec65d263614a
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2021-11-24 00:08:52 +0900

    Update NILayer.java

--- StaticRouter/src/router/NILayer.java
+++ StaticRouter/src/router/NILayer.java
@@ -14,7 +14,7 @@ import org.jnetpcap.packet.PcapPacket;
 
 public class NILayer extends BaseLayer {
 
-	private ArrayList<PcapIf> allDevs = new ArrayList<PcapIf>();
+	public static ArrayList<PcapIf> allDevs = new ArrayList<PcapIf>();
 	private StringBuilder errbuf = new StringBuilder();
 	
 	private ArrayList<Pcap> adapters;
@@ -42,25 +42,9 @@ public class NILayer extends BaseLayer {
 			return;
 		}
 	}
-	
-	public boolean setDevice(int idx, int snaplen, int flags, int timeout) {
-		PcapIf device = allDevs.get(idx);
-		pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
-		try {
-			interfaceID = idx;
-			macAddress = device.getHardwareAddress();
-			ipAddress = device.getAddresses().get(0).getAddr().getData();
-			System.out.println(MacToStr(macAddress));
-		} catch (IOException e) { System.out.println("주소를 찾을 수 없습니다."); }
-
-		if (pcap == null) {
-			System.out.printf("pcap 객체 생성실패 - %s\n", errbuf.toString());
-			return false;
-		}	return true;
-	}
     
 	public boolean Receive() {
-		Receive_Thread thread = new Receive_Thread(this.pcap, this.GetUpperLayer(0));
+		Receive_Thread thread = new Receive_Thread(this.adapters, this.GetUpperLayer(0));
 		Thread obj = new Thread(thread);
 		obj.start();
 		return true;
@@ -97,12 +81,14 @@ class Receive_Thread implements Runnable {
 		PcapHeader header = new PcapHeader(JMemory.POINTER);
 		JBuffer buff = new JBuffer(JMemory.POINTER);
 		while (true) {
-			for (var adapter : adapters){
+			for (int i = 0; i < adapters.size(); i++){
+				var adapter = adapters.get(i);
 				if (adapter.nextEx(header, buff) != Pcap.NEXT_EX_OK) continue;
 				
 				var packet = new PcapPacket(header, buff);
 				packet.scan(id);
 				data = packet.getByteArray(0, packet.size());
+				var device = NILayer.allDevs.get(i);
 				srcMacAddress = device.getHardwareAddress();
 				srcIpAddress = device.getAddresses().get(0).getAddr().getData();
 				UpperLayer.Receive(data);
